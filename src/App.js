25b276bdafb1bbd6ea6103d2336c164103343ec2
thi han <thihan245@gmail.com>
2024-04-04 11:39:48 +0630

    delete-todo

--- src/App.js
+++ src/App.js
@@ -1,7 +1,7 @@
 import './reset.css';
 import './App.css';
-import TodoForm from './components/TodoForm.js'
-import TodoList from './components/TodoList.js'
+import TodoForm from './components/TodoForm.js';
+import TodoList from './components/TodoList.js';
 import CheckAllAndRemaining from './components/CheckAllAndRemaining.js';
 import TodoFilters from './components/TodoFilters.js';
 import ClearCompletedBtn from './components/ClearCompletedBtn.js';
@@ -9,40 +9,49 @@ import { useEffect, useState } from 'react';
 
 function App() {
 
-  let [ todos , setTodos ] = useState([])
+  let [ todos, setTodos ] = useState([]);
 
-  useEffect(()=>{
+  useEffect(() => {
     fetch('http://localhost:3003/todo')
-    .then(res=>res.json())
-    .then((todos)=>{
+    .then(res => res.json())
+    .then((todos) => {
       setTodos(todos)
     })
-  },[])
+  }, [])
 
 
   let addTodo = (todo) => {
-    // update server side
-    fetch('http://localhost:3003/todo',{
+    fetch('http://localhost:3003/todo' ,{
       method : "POST",
-     headers: {
-      "Content-Type": "db/json",
-     },
-     body : JSON.stringify(todo)
+      headers: {
+        'Content-Type': 'application/json'
+      },
+      body : JSON.stringify(todo)
     })
-    //update data at client side
     setTodos(prevState => [...prevState,todo])
+  }
+
+  let deleteTodo = (todoId) => {
+    fetch(`http://localhost:3003/todo/${todoId}`,{
+      method : "DELETE"
+    })
+    setTodos(prevState => {
+      return prevState.filter(todo => {
+        return todo.id !== todoId
+      });// [todo,todo]
+    })
   }
 
   return (
     <div className="todo-app-container">
       <div className="todo-app">
         <h2>Todo App</h2>
-        <TodoForm addTodo = {addTodo} />
-        <TodoList todos = {todos}/>
-        <CheckAllAndRemaining />
+        <TodoForm addTodo={addTodo}/>
+        <TodoList todos={todos} deleteTodo={deleteTodo}/>
+        <CheckAllAndRemaining/>
         <div className="other-buttons-container">
-          <TodoFilters />
-          <ClearCompletedBtn />
+          <TodoFilters/>
+          <ClearCompletedBtn/>
         </div>
       </div>
     </div>
